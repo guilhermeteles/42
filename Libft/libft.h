19d2f6aeda587ff4ea878d7a6c70d33dc98e2eee
guilhermeteles <guilhermeteles@gmail.com>
2020-10-06 21:38:10 -0300

    v1

---
+++ Libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmota <gmota@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/06 22:27:38 by gmota             #+#    #+#             */
+/*   Updated: 2020/10/06 22:27:38 by gmota            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+void	*ft_memset(void *str, int c, size_t n);
+
+#endif
--- test.c
+++
@@ -1,6 +0,0 @@
-/*The C library function void *memset(void *str, int c, size_t n) copies the character c (an unsigned char) to the first n characters of the string pointed to, by the argument str.*/
-
-void *memset(void *str, int c, size_t n)
-{
-    return 0;
-}
