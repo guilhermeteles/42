a8bf9c2a23cf9e797ab607f5e888d3e68b11d75f
guilhermeteles <guilhermeteles@gmail.com>
2020-10-06 22:54:29 -0300

    v11.0

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -1,20 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmota <gmota@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/06 22:27:38 by gmota             #+#    #+#             */
-/*   Updated: 2020/10/06 22:27:38 by gmota            ###   ########.fr       */
+/*   Created: 2020/10/06 22:53:07 by gmota             #+#    #+#             */
+/*   Updated: 2020/10/06 22:53:07 by gmota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-
-void	*ft_memset(void *str, int c, size_t n);
-
-#endif
+void	*ft_memset(void *str, int c, size_t n)
+{
+    return 0;
+}
